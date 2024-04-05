b419b1e904f420068267608dac66f072f089df40
Heitor Leal <heitorlsms@gmail.com>
2024-04-05 00:34:11 -0300

    Atividade Pw

---
+++ exercicios/ex.1(get)/resp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado formulário</h1>
+    </header>
+    <main>
+        <?php
+            $_num1=$_GET['num1'];
+            $_num2 = $_GET['num2'];
+
+
+            echo"<p>Primeiro número: $_num1</p>";
+        ?>
+    </main>
+</body>
+</html>
+<h1>Resultado formulário</h1>
